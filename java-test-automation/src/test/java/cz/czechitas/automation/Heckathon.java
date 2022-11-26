f604966c0c342cddadd775a567d3fe2cf87a3996
KZuchnicka <116957788+KZuchnicka@users.noreply.github.com>
2022-11-26 17:00:35 +0100

    Add files via upload

---
+++ java-test-automation/src/test/java/cz/czechitas/automation/Heckathon.java
@@ -0,0 +1,43 @@
+package cz.czechitas.automation;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Example test class for functionality showcase
+ *
+ * @author Jiri Koudelka
+ * @since 1.0.0
+ */
+final class Heckathon extends TestRunner {
+
+    @Test
+    void OverPrihlaseniUzivatele() {
+        prohlizec.prihlasovani.klikniNaTlacitkoSignIn();
+        prohlizec.prihlasovani.vyplnEmail("kzuchnicka@seznam.cz");
+        prohlizec.prihlasovani.vyplnHeslo("Katka123");
+        prohlizec.prihlasovani.provedPrihlaseni();
+        overeni.overPrihlaseniUzivatele();
+    }
+
+    @Test
+    void OverOdhlaseniUzivatele() {
+        prohlizec.prihlasovani.klikniNaTlacitkoSignIn();
+        prohlizec.prihlasovani.vyplnEmail("kzuchnicka@seznam.cz");
+        prohlizec.prihlasovani.vyplnHeslo("Katka123");
+        prohlizec.prihlasovani.provedPrihlaseni();
+        prohlizec.prihlasovani.provedOdhlaseni();
+        overeni.overOdhlaseniUzivatele();
+    }
+
+    @Test
+    void ZobrazDetailPokoje() {
+        prohlizec.sekceOurRooms.klikniNaBookNow();
+        overeni.overDetailPokoje();
+    }
+
+
+
+
+
+
+}
